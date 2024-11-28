700143628a41e9a10c46987bca06cb3d2e8f58d6
HongShi-oneapi <hongshi-oneapi@outlook.com>
2024-11-28 13:19:07 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";//your website url
+  let target = "https://gist.githubusercontent.com/HongShi-oneapi/b8ee3415826ac31a12387d95e37a70d1/raw/clash.yaml";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
